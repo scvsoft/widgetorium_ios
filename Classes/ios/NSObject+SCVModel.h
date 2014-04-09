be9d65db85d6ad46e5dae7c024f4c4894dd0335a
Emanuel <emanuel@MacBookAir-Emanuel.local>
2014-04-09 15:37:04 -0300

    Fixed issues with model population, parsing snake_style properties and header files.

--- Classes/ios/NSObject+SCVModel.h
+++ Classes/ios/NSObject+SCVModel.h
@@ -10,4 +10,30 @@
 
 @interface NSObject (SCVModel) <SCVModel>
 
+//
++ (instancetype)populatedObjectWithObject:(id)object
+                                  options:(NSDictionary *)options
+                                    error:(NSError **)error
+                                   parent:(id)parent
+                                parentKey:(NSString *)parentKey;
+
+// Translate key
+- (NSString *)keyForDictionaryKey:(NSString *)key;
+
+// Translate values
+// Useful to translate strings to enums
+- (NSNumber *)numberWithValue:(id)value forKey:(NSString *)key;
+
+// NSDate
+// Default callback instanciating a NSDate
+- (NSDate *)dateWithString:(NSString *)string
+                    forKey:(NSString *)key
+                   options:(NSDictionary *)options
+                     error:(NSError *__autoreleasing*)error;
+
+// NSArray
+- (Class)classForArrayPropertyWithKey:(NSString *)key
+                              options:(NSDictionary *)options
+                                error:(NSError **)error;
+
 @end
